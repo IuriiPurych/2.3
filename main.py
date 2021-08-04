ea661a47de5c5e6322c68db962f6e5e38c87afee
iuriipurych <iurii.purych@gmail.com>
2021-08-04 23:06:45 +0300

    final

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class Foo:
+    pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Bar:
+    pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class One:
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class Two:
+    pass
+
+
+class Some(Foo, Bar, One, Two):
+    pass
+
+
+var = Some.__mro__
+print(var)
